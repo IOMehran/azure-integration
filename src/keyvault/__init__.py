bd66d330008a4f077e88b39f032dc45176ea8a8a
mehran <iomehran@riseup.net>
2021-04-29 13:49:23 +0430

    feat(vault): create get secrets and get secrets values methods.
    this methods will help to get secrets you need by giving a dict contains all of their names and versions.

--- src/keyvault/__init__.py
+++ src/keyvault/__init__.py
@@ -15,6 +15,59 @@ class SecretClient:
         self._check_env_variables()
         return self.client.get_secret(name=secret_name, version=secret_version)
 
+    def get_secrets(self, secrets: dict) -> dict:
+        """
+        looks into secrets dict and puts each key equals to KeyVaultSecret object of given version.
+
+        Parameters
+        ----------
+        secrets: dict
+            dict object of secret_names and secret_versions that you wanna fetch.
+            input dict should be something like this:
+            {
+                "SECRET_NAME": "SECRET_VERSION",
+                "SECOND_SECRET_NAME": "SECOND_SECRET_VERSION"
+            }
+
+        Returns
+        -------
+        dict
+            a dict object of secret_names and their values typed KeyVaultSecret.
+            NOTE: this method modifies given secrets variable, but also returns result.
+        """
+
+        for k, v in secrets.items():
+            if type(secrets[k]) == KeyVaultSecret:
+                continue
+            secrets[k] = self.get_secret(k, v)
+        return secrets
+
+    def get_secrets_values(self, secrets: dict) -> dict:
+        """
+        looks into secrets dict and puts each key equals to str value of given version.
+
+        Parameters
+        ----------
+        secrets: dict
+            dict object of secret_names and secret_versions that you wanna fetch.
+            input dict should be something like this:
+            {
+                "SECRET_NAME": "SECRET_VERSION",
+                "SECOND_SECRET_NAME": "SECOND_SECRET_VERSION"
+            }
+
+        Returns
+        -------
+        dict
+            a dict object of secret_names and their values typed str.
+            NOTE: this method modifies given secrets variable, but also returns result.
+        """
+
+        items = self.get_secrets(secrets)
+        for k, v in items.items():
+            items[k] = v.value
+        return items
+
     @staticmethod
     def _check_env_variables() -> bool:
         if not getenv('AZURE_CLIENT_ID') or not getenv('AZURE_CLIENT_SECRET') or not getenv('AZURE_TENANT_ID'):
