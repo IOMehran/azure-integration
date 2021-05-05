0f92674bcce0fba1e0f4b455d87c1dff38d13269
mehran <mehran@keyleadhealth.com>
2021-05-05 11:44:05 +0430

    chore: update version to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="azure-integration",  # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="Mehran Zolghadr",
     author_email="mehran@keyleadhealth.com",
     description="Microsoft Azure Integration",
